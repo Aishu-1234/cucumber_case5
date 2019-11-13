cc5626a379eb12a5bd1c74292cd4c7d7fb061b4f
Aishu-1234 <57519842+Aishu-1234@users.noreply.github.com>
2019-11-13 15:07:50 +0530

    Add files via upload

---
+++ src/test/java/skeleton/Stepdefs.java
@@ -0,0 +1,49 @@
+package skeleton;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefs {
+	WebDriver driver;
+	@Given("user launches chromeBrowser And navigate to application")
+	public void user_launches_chromeBrowser_And_navigate_to_application() {
+		System.setProperty("webdriver.ie.driver","C:\\NexGen Testing Stream\\Drivers\\IEDriverServer_Win32_3.14.0\\IEDriverServer.exe");
+		driver=new InternetExplorerDriver();
+		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 driver.navigate().to("http://newtours.demoaut.com/");
+	    
+	}
+
+	@When("user enter username as {string} in username field")
+	public void user_enter_username_as_in_username_field(String un) {
+		driver.findElement(By.name("userName")).sendKeys(un);
+	    
+	}
+
+	@When("user enter password as {string} in password1 field")
+	public void user_enter_password_as_in_username_field(String pwd) {
+		driver.findElement(By.name("password")).sendKeys(pwd);
+	   
+	}
+
+	@When("click Signin")
+	public void click_Signin() {
+		driver.findElement(By.name("login")).click();
+	    
+	}
+
+	@Then("validates Login success")
+	public void validates_Login_success() {
+		Assert.assertTrue(driver.getTitle().contains("Find a Flight"));
+	    
+	}
+}
